c8af91996d34626a79407aa059420dcae60faa69
uroge <67282212+uroge@users.noreply.github.com>
2021-07-27 13:52:08 +0200

    Create header main and header secondary and style it

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
 import './Header.scss';
 
-const Header = () => {
-    return <h1>Header</h1>;
+const Header = (props) => {
+    console.log(props);
+    return (
+        <div className="header">
+            <div className="header-main">
+                <h1 className="header__logo">
+                    <span>DEMO</span>
+                    Streaming
+                </h1>
+                <div className="header__buttons">
+                    <a href="/" className="header__buttons-login">Login</a>
+                    <a href="/" className="header__buttons-trial">Start free trial</a>
+                </div>
+            </div>
+            <div className="header-secondary">
+                <h1>Popular Titles</h1>
+            </div>
+        </div>
+    );
 }
 
 export default Header;
