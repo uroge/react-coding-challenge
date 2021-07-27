0aab4f5a8a1484656f992a008b96f6059ad8b0d4
uroge <67282212+uroge@users.noreply.github.com>
2021-07-27 15:32:52 +0200

    Create home page and style it

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,28 @@
 import React from 'react';
 import './Home.scss';
 
+import { RiMovie2Fill } from 'react-icons/ri';
+import { MdLocalMovies } from 'react-icons/md';
+
+import { Link } from 'react-router-dom';
+
 const Home = () => {
-    return <h1>Home</h1>;
+    return (
+        <div className="home">
+            <Link to="/series">
+                <div className="home__card">
+                    <RiMovie2Fill className="home__icon" />
+                    <p>Popular Series</p>
+                </div>
+            </Link>
+            <Link to="/movies">
+                <div className="home__card">
+                    <MdLocalMovies className="home__icon" />
+                    <p>Popular Movies</p>
+                </div>
+            </Link>
+        </div>
+    );
 }
 
 export default Home;
