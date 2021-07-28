71563fa7e7e7ff7564c03e423600af84918e6b12
uroge <67282212+uroge@users.noreply.github.com>
2021-07-28 09:06:29 +0200

    Fetch data from local json file

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: '../../feed/sample.json'
+    baseURL: '../../feed/'
 });
 
 export default axiosInstance;
--- src/pages/Series/Series.jsx
+++ src/pages/Series/Series.jsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Series.scss';
 
+import axios from '../../axios/axios';
+
 const Series = () => {
-    return <h1>Series</h1>;
+    useEffect(() => {
+        axios.get('sample.json')
+        .then(response => {
+            console.log(response.data)
+        })
+        .catch(error => console.log(error));
+    }, []);
+
+    return (
+        <h1>Series</h1>
+    );
 }
 
 export default Series;
