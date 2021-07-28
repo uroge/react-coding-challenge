3f4f702cf18c7fcf03684f71617d00da40ac2675
uroge <67282212+uroge@users.noreply.github.com>
2021-07-28 10:31:23 +0200

    Create store with reducers and actions

--- src/index.js
+++ src/index.js
@@ -4,9 +4,18 @@ import './sass/main.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './store/reducers/rootReducer';
+import thunk from 'redux-thunk';
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
